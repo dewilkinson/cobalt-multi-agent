057310fc31f810078df96212f2499042406b9e08
dewilkinson <dwilkinson@me.com>
2026-03-19 22:44:27 -0400

    initial commit

---
+++ bootstrap.bat
@@ -0,0 +1,28 @@
+@echo off
+SETLOCAL ENABLEEXTENSIONS
+
+REM Check if argument is dev mode
+SET MODE=%1
+IF "%MODE%"=="--dev" GOTO DEV
+IF "%MODE%"=="-d" GOTO DEV
+IF "%MODE%"=="dev" GOTO DEV
+IF "%MODE%"=="development" GOTO DEV
+
+:PROD
+echo Starting DeerFlow in [PRODUCTION] mode...
+start .\.venv\Scripts\python.exe server.py
+cd web
+start C:\Users\rende\AppData\Local\pnpm\pnpm.CMD start
+REM Wait for user to close
+GOTO END
+
+:DEV
+echo Starting DeerFlow in [DEVELOPMENT] mode...
+start .\.venv\Scripts\python.exe server.py --reload
+cd web
+start C:\Users\rende\AppData\Local\pnpm\pnpm.CMD dev
+REM Wait for user to close
+pause
+
+:END
+ENDLOCAL
